d19502e5cfa0e626c515f7e940d6defa9f176bdc
RaghunandhanG <143869346+RaghunandhanG@users.noreply.github.com>
2024-04-28 03:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import random
 st.title("SGPA CALCULATOR")
 
 uploaded_file , res = upload_file()
-if res and st.button("Submit"):
+if res:
             file_path = os.path.join("uploads", uploaded_file.name)
             upload_file_in_db(uploaded_file , file_path)
             text = read_file(file_path)
