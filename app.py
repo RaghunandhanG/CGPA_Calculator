49b6a1a37afe472b2eb135a0e3f17948cec6ebb7
RaghunandhanG <143869346+RaghunandhanG@users.noreply.github.com>
2024-04-28 03:16:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,24 @@ from PyPDF2 import PdfReader
 from app_functions import *
 import os
 import time
+import random
 
 st.title("SGPA CALCULATOR")
-    st.session_state.load_state = False
+
 uploaded_file , res = upload_file()
 if res:
             file_path = os.path.join("uploads", uploaded_file.name)
             upload_file_in_db(uploaded_file , file_path)
             text = read_file(file_path)
-
             info = get_info(text)
-            course_names = get_course_names(text)
+            course_names , name , reg_no = get_course_names(text)
+
+            reg_no_color = random.choice(["red", "green", "blue","magenta", "yellow"])
+
+            reg_no_font_size = "25px"
+            name_font_size = "35px"
+            additional_text_font_size = "25px"
+            st.write(f'<h1> <span style="font-size:{additional_text_font_size};">Hi,</span><span style="font-size:{name_font_size}; color:{"black"};">{name}</span> <span style="font-size:{reg_no_font_size}; color:{reg_no_color};">{reg_no.replace('"' , "")}</span></h1>', unsafe_allow_html=True)
             grades , credits = find_grade_and_credit(info , course_names)
 
             selected_course_names ,credits, grades , res = select_course_names(course_names , credits , grades)
@@ -31,6 +38,17 @@ if res:
             data = generate_df(weights , course_names , sum , grades)
 
             plot(data)
+            st.write("## Developer Info")
+            st.write(
+        """
+        - **Developer**: Raghunandhan G 
+        - **Contact**: 
+            - Email: [raghunandhan.22me@kct.ac.in](mailto:raghunandhan.22me@kct.ac.in)
+            - LinkedIn: [Raghunandhan G](https://www.linkedin.com/in/raghunandhan-g-92a28025a)
+        - **GitHub**: [RaghunandhanG GitHUB](https://github.com/RaghunandhanG)
+        """
+    )
+
 
 
 
