60334daa5435922a08e4563862112139d6f78282
RaghunandhanG <143869346+RaghunandhanG@users.noreply.github.com>
2024-04-27 23:31:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ if res:
             file_path = os.path.join("uploads", uploaded_file.name)
             upload_file_in_db(uploaded_file , file_path)
             text = read_file(file_path)
+
             info = get_info(text)
             course_names = get_course_names(text)
-            grades , credits = find_grade_and_credit(info)
+            grades , credits = find_grade_and_credit(info , course_names)
+
             selected_course_names ,credits, grades , res = select_course_names(course_names , credits , grades)
             
                  
-            sgpa , total_credits ,weights , back_logs = calculate_sgpa(grades , credits)
-            data = generate_df(weights , course_names)
-            plot(data)
+            sgpa , total_credits ,weights , back_logs , sum = calculate_sgpa(grades , credits)
 
             with st.spinner('Calculating...'):
                 time.sleep(3)
@@ -28,6 +28,9 @@ if res:
             col2.metric(f":green[Credits Completed]",total_credits)
             col3.metric(f':red[Back Logs]' , back_logs)
                 #generate_report(weights)
+            data = generate_df(weights , course_names , sum , grades)
+
+            plot(data)
 
 
 
