01515f5dc42ef15736ca6279838453c6888a3d6f
RaghunandhanG <143869346+RaghunandhanG@users.noreply.github.com>
2024-04-28 03:30:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,14 @@ if res:
             info = get_info(text)
             course_names , name , reg_no = get_course_names(text)
 
-            reg_no_color = random.choice(["red", "green", "blue","magenta", "yellow"])
+            reg_no_color = random.choice([
+    "red", "green", "blue", "orange", "purple", "pink", "brown", 
+    "teal", "navy", "olive", "maroon", "lime", "aqua", "fuchsia", 
+    "silver", "gray", "black", "white", "coral", "indigo", "gold",
+    "violet", "turquoise", "salmon", "khaki", "orchid", "steelblue",
+    "tomato", "thistle", "sienna", "rosybrown", "slategray", "peru"
+]
+)
             reg_no = reg_no.replace('"' , "")
             reg_no_font_size = "25px"
             name_font_size = "35px"
