d2e258e51a794224054c4d7e259da870dc7d15c0
RaghunandhanG <143869346+RaghunandhanG@users.noreply.github.com>
2024-04-28 03:37:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if res:
                 
      except:
             st.toast("Try again with a valid file!!")
-        reg_no_color = random.choice([
+     reg_no_color = random.choice([
     "red", "green", "blue", "orange", "purple", "pink", "brown", 
     "teal", "navy", "olive", "maroon", "lime", "aqua", "fuchsia", 
     "silver", "gray", "black", "white", "coral", "indigo", "gold",
@@ -26,30 +26,30 @@ if res:
     "tomato", "thistle", "sienna", "rosybrown", "slategray", "peru"
 ]
 )
-        reg_no = reg_no.replace('"' , "")
-        reg_no_font_size = "25px"
-        name_font_size = "35px"
-        additional_text_font_size = "25px"
-        st.write(f'<h1><span style="font-size:{additional_text_font_size};">Hi,</span><span style="font-size:{name_font_size}; color:black;">{name}</span> <span style="font-size:{reg_no_font_size}; color:{reg_no_color};">{reg_no}</span></h1>', unsafe_allow_html=True)
-        grades , credits = find_grade_and_credit(info , course_names)
+     reg_no = reg_no.replace('"' , "")
+     reg_no_font_size = "25px"
+     name_font_size = "35px"
+     additional_text_font_size = "25px"
+     st.write(f'<h1><span style="font-size:{additional_text_font_size};">Hi,</span><span style="font-size:{name_font_size}; color:black;">{name}</span> <span style="font-size:{reg_no_font_size}; color:{reg_no_color};">{reg_no}</span></h1>', unsafe_allow_html=True)
+     grades , credits = find_grade_and_credit(info , course_names)
 
-        selected_course_names ,credits, grades , res = select_course_names(course_names , credits , grades)
+     selected_course_names ,credits, grades , res = select_course_names(course_names , credits , grades)
             
                  
-        sgpa , total_credits ,weights , back_logs , sum = calculate_sgpa(grades , credits)
+     sgpa , total_credits ,weights , back_logs , sum = calculate_sgpa(grades , credits)
 
-        with st.spinner('Calculating...'):
-            time.sleep(3)
-        col1, col2 ,col3= st.columns(3)
-        col1.metric(f":blue[SGPA]",sgpa)
-        col2.metric(f":green[Credits Completed]",total_credits)
-        col3.metric(f':red[Back Logs]' , back_logs)
+     with st.spinner('Calculating...'):
+          time.sleep(3)
+     col1, col2 ,col3= st.columns(3)
+     col1.metric(f":blue[SGPA]",sgpa)
+     col2.metric(f":green[Credits Completed]",total_credits)
+     col3.metric(f':red[Back Logs]' , back_logs)
 
-        data = generate_df(weights , course_names , sum , grades)
+     data = generate_df(weights , course_names , sum , grades)
 
-        plot(data)
-        st.write("## Developer Info")
-        st.write(
+     plot(data)
+     st.write("## Developer Info")
+     st.write(
         """
         - **Developer**: Raghunandhan G 
         - **Contact**: 
