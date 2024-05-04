02a38d4293a574e63a048c8706263796abf06a80
RaghunandhanG <143869346+RaghunandhanG@users.noreply.github.com>
2024-05-04 18:56:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,19 +24,18 @@ if res:
             reg_no = reg_no.replace('"' , "")
             st.write(f'<h1> <span style="font-size:{additional_text_font_size};">Hi,</span><span style="font-size:{name_font_size};">{name}</span> <span style="font-size:{reg_no_font_size}; color:{reg_no_color};">{reg_no}</span></h1>', unsafe_allow_html=True)
             grades , credits = find_grade_and_credit(info , course_names)
-
             selected_course_names ,credits, grades , res = select_course_names(course_names , credits , grades)
             
-                 
-            sgpa , total_credits ,weights , back_logs , sum = calculate_sgpa(grades , credits)
+            sgpa , total_credits ,weights , back_logs , sum ,credits_completed= calculate_sgpa(grades , credits)
 
             with st.spinner('Calculating...'):
                 time.sleep(3)
-            col1, col2 ,col3= st.columns(3)
+            col1, col2 ,col3,col4= st.columns(4)
             col1.metric(f":blue[SGPA]",sgpa)
-            col2.metric(f":green[Credits Completed]",total_credits)
-            col3.metric(f':red[Back Logs]' , back_logs)
-                #generate_report(weights)
+            col2.metric(f":green[Total Credits]",total_credits)
+            col3.metric(f':blue[Credits Completed]' , credits_completed)
+            col4.metric(f':red[Back Logs]' , back_logs)
+
             data = generate_df(weights , course_names , sum , grades)
 
             plot(data)
@@ -50,14 +49,3 @@ if res:
         - **GitHub**: [RaghunandhanG GitHUB](https://github.com/RaghunandhanG)
         """
     )
-
-
-
-
-
-
-
-
-
-
- 
