2fc4f7566860dcd6beb9d9c2f9f216e547433e73
RaghunandhanG <143869346+RaghunandhanG@users.noreply.github.com>
2024-07-10 22:38:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,34 +36,35 @@ if uploaded:
 
             flat_list = list(chain.from_iterable(course_names))
             selected_course_names ,credits, grades , res = select_course_names(course_names , credits , grades)
-            sgpa , total_credits ,weights , back_logs , sum ,credits_completed= calculate_sgpa(grades , credits)
+            if res:
+                sgpa , total_credits ,weights , back_logs , sum ,credits_completed= calculate_sgpa(grades , credits)
 
-            reg_no_color = random.choice(["red", "green", "blue","magenta", "yellow"])
+                reg_no_color = random.choice(["red", "green", "blue","magenta", "yellow"])
 
-            reg_no_font_size = "25px"
-            name_font_size = "35px"
-            additional_text_font_size = "25px"
-            reg_no = reg_no.replace('"' , "")
-            st.write(f'<h1> <span style="font-size:{additional_text_font_size};">Hi,</span><span style="font-size:{name_font_size};">{name}</span> <span style="font-size:{reg_no_font_size}; color:{reg_no_color};">{reg_no}</span></h1>', unsafe_allow_html=True)
+                reg_no_font_size = "25px"
+                name_font_size = "35px"
+                additional_text_font_size = "25px"
+                reg_no = reg_no.replace('"' , "")
+                st.write(f'<h1> <span style="font-size:{additional_text_font_size};">Hi,</span><span style="font-size:{name_font_size};">{name}</span> <span style="font-size:{reg_no_font_size}; color:{reg_no_color};">{reg_no}</span></h1>', unsafe_allow_html=True)
 
-            with st.spinner('Calculating...'):
-                time.sleep(1)
-            col1, col2 ,col3,col4= st.columns(4)
-            col1.metric(f":blue[SGPA]",sgpa)
-            col2.metric(f":green[Total Credits]",total_credits)
-            col3.metric(f':blue[Credits Completed]' , credits_completed)
-            col4.metric(f':red[Back Logs]' , back_logs)
+                with st.spinner('Calculating...'):
+                    time.sleep(1)
+                col1, col2 ,col3,col4= st.columns(4)
+                col1.metric(f":blue[SGPA]",sgpa)
+                col2.metric(f":green[Total Credits]",total_credits)
+                col3.metric(f':blue[Credits Completed]' , credits_completed)
+                col4.metric(f':red[Back Logs]' , back_logs)
 
-            data = generate_df(weights , course_names , sum , grades)
+                data = generate_df(weights , course_names , sum , grades)
 
-            st.write("## Developer Info")
-            st.write(
-        """
-        - **Developer**: Raghunandhan G 
-        - **Contact**: 
-            - Email: [raghunandhan.22me@kct.ac.in](mailto:raghunandhan.22me@kct.ac.in)
-            - LinkedIn: [Raghunandhan G](https://www.linkedin.com/in/raghunandhan-g-92a28025a)
-        - **GitHub**: [RaghunandhanG GitHUB](https://github.com/RaghunandhanG)
-        """
-    )
-            st.write("**Made with ❤️ in iQube**")
+                st.write("## Developer Info")
+                st.write(
+            """
+            - **Developer**: Raghunandhan G 
+            - **Contact**: 
+                - Email: [raghunandhan.22me@kct.ac.in](mailto:raghunandhan.22me@kct.ac.in)
+                - LinkedIn: [Raghunandhan G](https://www.linkedin.com/in/raghunandhan-g-92a28025a)
+            - **GitHub**: [RaghunandhanG GitHUB](https://github.com/RaghunandhanG)
+            """
+        )
+                st.write("**Made with ❤️ in iQube**")
